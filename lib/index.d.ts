eec7865f4c98bddd1883e22a6487713a7a0b1fa0
Desmond Kofi Adusei <desdhi24@gmail.com>
2024-07-31 21:16:43 +0000

    chore: working on the type extractor

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,1 @@
-import "./tailwind.css";
 export * from "../lib/components/ui";
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({ tsconfigPath: "./tsconfig.json", rollupTypes: true }),
+  ],
 
   build: {
     lib: {
