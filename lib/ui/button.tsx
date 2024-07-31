7af66d166f8ed4fe374549954225243f177823f2
Desmond Kofi Adusei <desdhi24@gmail.com>
2024-07-31 10:36:24 +0000

    chore: worked on my first publish

---
+++ lib/ui/button.tsx
@@ -0,0 +1,5 @@
+
+
+export const Button = () => {
+  return <div style={{ background: "red", color: "white" }}>Button</div>;
+};
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Button } from "chip-ui";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button></Button>
+    </>
+  );
+}
+
+export default App;
