43c608a5f25e9de4a0ff3a5cc1f6aa283c86d5ff
Desmond Kofi Adusei <desdhi24@gmail.com>
2024-08-01 16:57:14 +0000

    chore: publish

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,21 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), dts({ rollupTypes: true })],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./lib"),
+    },
+  },
+  // esbuild: {
+  //   jsxFactory: "React.createElement",
+  //   jsxFragment: "React.Fragment",
+  //   jsxInject: `import React from 'react'`,
+  // },
+  optimizeDeps: {
+    esbuildOptions: {
+      jsx: "automatic",
+    },
+  },
   build: {
     // library entry and output settings
     lib: {
